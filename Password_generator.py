cce197b95ce8d739c743c9d6dfcca927de67f65d
Venkateshvenki404224 <75518037+Venkateshvenki404224@users.noreply.github.com>
2020-12-13 04:00:14 +0530

    Add files via upload

---
+++ Password_generator.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+from random import randint
+small='qwertyuiopasdfghjklzxcvbnm'
+large=small.upper()
+symbols='!£$%^&_-+=;:><?*()'
+numbers='1234567890'
+pass_word=small+large+symbols+numbers
+pass_length=int(input("Enter the Password Length You want:"))
+password=""
+length=0
+if pass_length<8:
+	print("The Mininmum length of Password Should be 8:")
+else:		
+	while length < pass_length:
+		password=password+pass_word[randint(0,len(pass_word)-1)]
+		length +=1
+	print('Generated password is:',password)
