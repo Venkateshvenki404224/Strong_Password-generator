86f62e72bc6d768444beff0472ea7ca0f63ad032
Venkateshvenki404224 <75518037+Venkateshvenki404224@users.noreply.github.com>
2020-12-13 04:11:18 +0530

    Update Password_generator.py

--- Password_generator.py
+++ Password_generator.py
@@ -6,7 +6,7 @@ symbols='!£$%^&_-+=;:><?*()'
 numbers='1234567890'
 pass_word=small+large+symbols+numbers
 pass_length=int(input("Enter the Password Length You want:"))
-password=""
+password="\n"
 length=0
 if pass_length<8:
 	print("The Mininmum length of Password Should be 8:")
@@ -15,3 +15,7 @@ else:
 		password=password+pass_word[randint(0,len(pass_word)-1)]
 		length +=1
 	print('Generated password is:',password)
+	#Creating a file name called Password 
+f = open("Password.txt","a")
+f.write(password)#Writing the file 
+f.close()
